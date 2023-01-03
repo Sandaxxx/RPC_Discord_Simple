1b26e3a484ab9eae160ef409ac05459caa6ee5c0
Sandax <91079659+Sandaxxx@users.noreply.github.com>
2023-01-03 23:05:47 +0100

    Add files via upload

---
+++ Discord_RPC_sandax.py
@@ -0,0 +1,32 @@
+import time
+from colorama import init, Fore, Back, Style
+init()
+from pypresence import Presence
+
+
+# ID OF APPLICATION DEVELOPPER PORTAL DISCORD (https://discord.com/developers/applications/) 
+ClientID = '1055081302770061402'
+
+RPC = Presence(ClientID) 
+RPC.connect() # CONNECTION 
+
+print(Fore.BLUE+"[♠] "+Fore.WHITE+"> "+Fore.YELLOW+"Connecting...")
+print(Fore.MAGENTA)
+
+print(RPC.update(
+    state="𝗖𝗵𝗲𝗰𝗸 𝗠𝘆 𝗚𝗶𝘁𝗛𝘂𝗯 🚀", # Second Text
+        details="𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗽𝗲𝗱 𝗕𝘆 𝗦𝗮𝗻𝗱𝗮𝘅#𝟯𝟵𝟴𝟰 💻", # First Text
+            large_image="sandaxpick", # Big Image
+                 small_image="piquesmall", # Little image corner right
+                     large_text="Follow twitch vu que tu vois ça 🕵️‍♂️", # when your mouse hovers over the large image
+                            buttons=[{"label": "Discord 🌴", "url": "https://discord.gg/mDdGhkRSXx"}, {"label": "GitHub 👾", "url": "https://github.com/Sandaxxx"}],
+
+    start=time.time()))
+print()
+
+# CONNECT VALID
+print(Fore.BLUE+"[♠] "+Fore.WHITE+"> "+Fore.GREEN+'Success Connections ')
+
+# ON THE RPC ALL TIME
+while True: 
+    time.sleep(50) 
